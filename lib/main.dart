f405a2a5f8aa1cf04f6406d987fd57130a37703c
sgardoll <sgardoll@gmail.com>
2022-06-18 16:16:00 +1000

    Working FutureBuilder on it's own

--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,7 @@
 import 'dart:async';
 import 'dart:convert';
-import 'package:flutter_bootstrap/flutter_bootstrap.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-
-// class _TextStyleItem extends StatelessWidget {
-//   const _TextStyleItem({
-//     Key? key,
-//     required this.name,
-//     required this.style,
-//     required this.text,
-//   }) : super(key: key);
-//
-//   final String name;
-//   final TextStyle style;
-//   final String text;
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Padding(
-//       padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-//       child: Row(
-//         crossAxisAlignment: CrossAxisAlignment.center,
-//         children: [
-//           SizedBox(
-//             width: 72,
-//             child: Text(name, style: Theme.of(context).textTheme.caption),
-//           ),
-//           Expanded(
-//             child: Text(text, style: style),
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-// }
 
 Future<Album> fetchAlbum() async {
   var headers = {
@@ -54,18 +21,10 @@ Future<Album> fetchAlbum() async {
 
   if (res.statusCode == 200) {
     return Album.fromJson((jsonDecode(res.body) as List)[0]);
-    //return Album.fromJson(jsonDecode(res.body[0]));
-    //return Album.fromJson(jsonDecode(res.body));
   } else {
     throw Exception('Failed to load glucose data');
   }
-  // if (res.statusCode != 200) throw Exception('http.get error: statusCode= ${res.statusCode}');
-  // return c
 }
-
-//   var response = await http.get(
-//       Uri.parse('https://stucgm.herokuapp.com/api/v1/entries/sgv?token=mycgm-4eed72c0613bed6d'));
-//
 
 class Album {
   final String direction;
@@ -75,11 +34,6 @@ class Album {
   const Album({required this.direction, required this.sgv, required this.mmol});
 
   factory Album.fromJson(Map<String, dynamic> json) {
-    //factory Album.fromJson(List<dynamic> json) {
-
-    //var j=json;
-    //var d=json['direction'];
-
     return Album(
       sgv: json['sgv'],
       direction: json['direction'],
@@ -88,14 +42,19 @@ class Album {
   }
 }
 
+
+
+
+
+
 void main() => runApp(const MyApp());
 
 class MyApp extends StatefulWidget {
-  const MyApp({Key? key}) : super(key: key);
+   const MyApp({super.key});
 
-  @override
-  _MyAppState createState() => _MyAppState();
-}
+   @override
+   _MyAppState createState() => _MyAppState();
+ }
 
 class _MyAppState extends State<MyApp> {
   late Future<Album> futureAlbum;
@@ -106,37 +65,29 @@ class _MyAppState extends State<MyApp> {
     futureAlbum = fetchAlbum();
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'StuCGM',
-      theme: ThemeData(primarySwatch: Colors.green),
-      darkTheme: ThemeData(primarySwatch: Colors.grey),
-      home: Scaffold(
-        body: SafeArea(
-          minimum: EdgeInsets.all(20),
-          child: FutureBuilder<Album>(
-            future: futureAlbum,
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                var mmol = (snapshot.data!.sgv.toDouble()) / 18;
-
-                return Text(
-                    mmol.toStringAsFixed(1) + ' ' + snapshot.data!.direction);
-              } else if (snapshot.hasError) {
-                return Text('${snapshot.error}');
-              }
-
-              // By default, show a loading spinner.
-              return const CircularProgressIndicator();
-            },
-          ),
-        ),
-        backgroundColor: Colors.lightBlue[50],
-      ),
-
-    );
-
-
+@override
+Widget build(BuildContext context) {
+  return MaterialApp(
+       title: 'My CGM',
+      theme: ThemeData(
+          colorSchemeSeed: const Color(0xff6750a4), useMaterial3: true),
+  home: Scaffold(
+  //appBar: AppBar(
+ // title: const Text('Fetch Data Example'),
+ // ),
+  body: Center(
+  child: FutureBuilder<Album>(
+        future: futureAlbum,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            var mmol = (snapshot.data!.sgv.toDouble()) / 18;
+            return Text(
+              '${mmol}+${snapshot.data!.direction}');
+          } else if (snapshot.hasError) {
+            return Text('${snapshot.error}');
+          }
+          // By default, show a loading spinner.
+          return const CircularProgressIndicator();
+        },
+      ),),),);}
   }
-}
