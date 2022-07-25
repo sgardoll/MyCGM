1dd315deffe334dcb8f24abd565698cfc194e57e
sgardoll <sgardoll@gmail.com>
2022-07-25 10:07:01 +1000

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -49,19 +49,8 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       floatingActionButton: ExampleExpandableFab(),
-      // appBar: AppBar(
-      //   // Here we take the value from the MyHomePage object that was created by
-      //   // the App.build method, and use it to set our appbar title.
-      //   title: Text(widget.title),
-      // ),
       body:
       Center(
         // Center is a layout widget. It takes a single child and positions it
@@ -271,17 +260,6 @@ class ExampleExpandableFab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // appBar: AppBar(
-      //   title: const Text('Add Insultin FAB'),
-      // ),
-      // body:
-      // ListView.builder(
-      //   padding: const EdgeInsets.symmetric(vertical: 8.0),
-      //   itemCount: 25,
-      //   itemBuilder: (context, index) {
-      //     return FakeItem(isBig: index.isOdd);
-      //   },
-      // ),
       floatingActionButton: ExpandableFab(
         distance: 75.0,
         children: [
